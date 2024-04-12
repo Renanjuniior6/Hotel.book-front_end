0254a97e226a6602ff69bffd187d63dcfbb72d53
Renanjuniior6 <renanjrr679@gmail.com>
2024-04-12 19:29:35 -0300

    correções

--- Hotel.book/src/page/Login/index.jsx
+++ Hotel.book/src/page/Login/index.jsx
@@ -1,61 +1,86 @@
+import { yupResolver } from "@hookform/resolvers/yup"
 import React from "react"
-import * as Yup from 'yup'
-import { Button, Container, ContainerItens, ErrorMessage, Input, Label, SingLink, Title } from "./style"
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { toast } from 'react-toastify'
-import api from '../../services/api'
+import { useForm } from "react-hook-form"
+import { toast } from "react-toastify"
+import * as Yup from "yup"
+
+import api from "../../services/api"
+import {
+  Button,
+  Container,
+  ContainerItens,
+  ErrorMessage,
+  Input,
+  Label,
+  SingLink,
+  Title,
+} from "./style"
 
 function Login() {
-
   const schema = Yup.object().shape({
-    email: Yup.string().email("O e-mail é invalido").required("O e-mail é obrigatório"),
-    password: Yup.string().required("A senha é obrigatório").min(6, "A senha deve ter no minimo 6 caracteristicas"),
+    email: Yup.string()
+      .email("O e-mail é invalido")
+      .required("O e-mail é obrigatório"),
+    password: Yup.string()
+      .required("A senha é obrigatório")
+      .min(6, "A senha deve ter no minimo 6 caracteristicas"),
   })
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(schema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   })
 
-  const onSubmit = async clientData => {
+  const onSubmit = async (clientData) => {
     const { data } = await toast.promise(
-      api.post('sessions', {
+      api.post("sessions", {
         email: clientData.email,
-        password: clientData.password
+        password: clientData.password,
       }),
       {
-        pending: 'Verificando seus dados',
-        success: 'Tudo certo, seja bem-vindo 👌',
-        error: 'Verfique seu e-mail e senha 🤯'
-      }
+        pending: "Verificando seus dados",
+        success: "Tudo certo, seja bem-vindo 👌",
+        error: "Verfique seu e-mail e senha 🤯",
+      },
     )
-
   }
-
 
   return (
     <Container>
       <ContainerItens>
-      <Title>
+        <Title>
           <b>H</b>otel.Book
         </Title>
 
         <h1>Welcome !</h1>
 
-      <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <Label error={errors.email?.message}>Email</Label>
-        <Input typer="email" {...register("email")} error={errors.email?.message}/>
-        <ErrorMessage>{errors.name?.email}</ErrorMessage>
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Label error={errors.email?.message}>Email</Label>
+          <Input
+            typer="email"
+            {...register("email")}
+            error={errors.email?.message}
+          />
+          <ErrorMessage>{errors.name?.email}</ErrorMessage>
 
-        <Label error={errors.password?.message} >Senha</Label>
-        <Input typer="password" {...register("password")} error={errors.password?.message}/>
-        <ErrorMessage>{errors.password?.email}</ErrorMessage>
+          <Label error={errors.password?.message}>Senha</Label>
+          <Input
+            typer="password"
+            {...register("password")}
+            error={errors.password?.message}
+          />
+          <ErrorMessage>{errors.password?.email}</ErrorMessage>
 
-        <Button>Entrar</Button>
-      </form>
+          <Button>Entrar</Button>
+        </form>
 
-        <SingLink>Não tem conta? <a>Sing up</a> </SingLink>
-        </ContainerItens>
+        <SingLink>
+          Não tem conta? <a>Sing up</a>{" "}
+        </SingLink>
+      </ContainerItens>
     </Container>
   )
 }
--- Hotel.book/src/page/Login/style.js
+++ Hotel.book/src/page/Login/style.js
@@ -9,89 +9,89 @@ export const Title = styled.p`
 `
 
 export const Container = styled.div`
-  height:100vh;
-  width:100vw;
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  align-items:center;
-  justify-content:center;
-  `
+  align-items: center;
+  justify-content: center;
+`
 
 export const ContainerItens = styled.div`
   background: #023509;
-  box-shadow:0px 4px 15px rgba(74, 164, 226, 0.24);
-  border-radius:10px;
-  height:70%;
-  padding:25px 75px;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
+  box-shadow: 0px 4px 15px rgba(74, 164, 226, 0.24);
+  border-radius: 10px;
+  height: 70%;
+  padding: 25px 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-  h1{
-    font-style:normal;
-    font-weight:500;
-    font-size:24px;
-    line-height:28px;
+  h1 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
     color: #ffff;
     margin-bottom: 10px;
-    text-align:center;
-    margin-top:50px;
+    text-align: center;
+    margin-top: 50px;
   }
 `
 
 export const Label = styled.p`
-  font-style:normal;
-  font-weight:500;
-  font-size:12px;
-  line-height:14px;
-  color:#fff;
-  margin-top:28px;
-  margin-bottom:5px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  color: #fff;
+  margin-top: 28px;
+  margin-bottom: 5px;
 `
 
 export const Input = styled.input`
   width: 391.42px;
   height: 38.52px;
   background: #ffff;
-  box-shadow:3px 3px 10px rgba(74, 144, 226, 0.19);
-  border-radius:5px;
-  border:none
+  box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+  border-radius: 5px;
+  border: none;
   padding-left: 10px;
 `
 
 export const Button = styled.button`
-    width: 182.81px;
-    height: 56.53px;
-    background: red;
-    border-radius:28px;
-    border:none;
-    cursor: pointer;
-    margin-top: 50px;
-    margin-bottom:25px;
-    font-style:500;
-    font-size:16px;
-    line-height:19px;
-    text-align:center;
-    color:#ffff;
+  width: 182.81px;
+  height: 56.53px;
+  background: red;
+  border-radius: 28px;
+  border: none;
+  cursor: pointer;
+  margin-top: 50px;
+  margin-bottom: 25px;
+  font-style: 500;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  color: #ffff;
 
-    &::hover {
-      opacity:0.8;
-    }
+  &:hover {
+    opacity: 0.8;
+  }
 
-    &:active{
-      opacity:0.6;
-    }
+  &:active {
+    opacity: 0.6;
+  }
 `
 
 export const SingLink = styled.a`
-  font-style:normal;
-  font-weight:normal;
-  font-size:14px;
-  line-height:16px;
-  color:#ffff;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  color: #ffff;
 
-  a{
+  a {
     cursor: pointer;
-    text-decoration:underline;
+    text-decoration: underline;
   }
 `
 
